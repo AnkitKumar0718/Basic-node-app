57661721ed2d78939aee23763bb08af86919b12c
Ankit Kumar <ankitchauhandhoni@gmail.com>
2024-06-12 10:37:26 +0530

    added a change

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/',(req,res)=>{
     res.send("BASIC-NODE-APP")
 })
 
+app.get('/change',(req,res)=>{
+    res.status(200);
+    res.send("Changes made")
+});
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
     
